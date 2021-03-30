159c6734f0eab1823d6456adb11167de9bb22a4f
Seb <seb@debian10pca1>
2021-03-30 14:49:06 +0200

    add Orange Pi Zero support

--- api/mraa/types.h
+++ api/mraa/types.h
@@ -68,6 +68,7 @@ typedef enum {
     MRAA_ADLINK_LEC_AL_AI = 23,        /**< Adlink LEC-AL*/
     MRAA_UPXTREME = 24,             /**< The UPXTREME Board */
     MRAA_INTEL_ILK = 25,            /**< Intel Learning Kit */
+    MRAA_ORANGEPIZERO = 50,         /**< Orange Pi Zero */
     // USB platform extenders start at 256
     MRAA_FTDI_FT4222 = 256,         /**< FTDI FT4222 USB to i2c bridge */
 
